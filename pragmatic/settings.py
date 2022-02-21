c8843eedfa4a1147a547e1057facbf5dd18e2055
김홍명 <92701579+Devshowviz@users.noreply.github.com>
2022-02-21 16:12:51 +0900

    django course 11

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # reading .env file
 environ.Env.read_env(
-    env_file = os.path.join(BASE_DIR,'.env')
+    env_file = os.path.join(BASE_DIR, '.env')
 )
 
 
@@ -130,9 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
