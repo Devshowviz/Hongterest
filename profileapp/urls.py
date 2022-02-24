223e454f6968e1a0013efa4dfad712fe5644af7e
김홍명 <92701579+Devshowviz@users.noreply.github.com>
2022-02-24 15:30:40 +0900

    -django course 32

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 
-from profileapp.views import ProfileCreateView
+from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'profileapp'
 
 urlpatterns = [
    path('create/',ProfileCreateView.as_view(), name='create'),
-
+   path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 ]
